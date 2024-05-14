889c9044a290fe4314fe3815bf2d09e388f9c25b
Janar <jogijanar@gmail.com>
2024-05-14 19:50:56 +0300

    Adjust Root texts

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -5,9 +5,18 @@ import { Link as ChakraLink } from '@chakra-ui/react'
 export default function Root() {
 
   return (
-    <Box padding={8}>
+    <Box padding={8} maxWidth={800}>
       <Heading marginBottom={4}>Wurm Utilities</Heading>
-      <Box marginBottom={2}>Wurm Utilities is a collection of tools for the game Wurm Online.</Box>
+      <Box marginBottom={2}>{`
+        Wurm Utilities is a collection of tools and calculators for the game Wurm Online.
+        The tools are designed to help players with common tasks such as treasure hunting,
+        highway building, and bridge building.`}
+      </Box>
+      <Box marginBottom={2}>{`
+        This is a personal project under active development, there may be bugs and 
+        functionality may break or change. It is is not affiliated with 
+        Code Club AB or Wurm Online.`}
+      </Box>
       <List>
         <ListItem>
           <ChakraLink as={ReactRouterLink} to='/treasures' color='teal'>
@@ -17,6 +26,11 @@ export default function Root() {
         <ListItem>
           <ChakraLink as={ReactRouterLink} to='/highways' color='teal'>
             Highway Calculator
+          </ChakraLink>
+        </ListItem>
+        <ListItem>
+          <ChakraLink as={ReactRouterLink} to='/bridges' color='teal'>
+            Bridge Calculator
           </ChakraLink>
         </ListItem>
       </List>
