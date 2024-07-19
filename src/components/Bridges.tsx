bbb97871bfb777c804d637383705420f74d027f1
Janar <jogijanar@gmail.com>
2024-07-20 01:23:10 +0300

    Add bridge cost display

--- src/components/Bridges.tsx
+++ src/components/Bridges.tsx
@@ -6,28 +6,37 @@ import {
   FormLabel,
   SimpleGrid,
   Select,
-  Button
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Text,
 } from '@chakra-ui/react'
 import { useCallback, useState } from 'react'
 import { Link as ReactRouterLink } from 'react-router-dom'
 import LengthInput from './common/LengthInput'
 import NumberInput from './common/NumberInput'
-import { BridgeType } from '../util/Bridges'
+import { BridgeCost, BridgeType, calcBridgeCost } from '../util/Bridges'
+import { formatCode } from '../util/Common'
 
 export default function Bridges() {
   const [ length, setLength ] = useState<number>(0)
   const [ width, setWidth ] = useState<number>(1)
   const [ bridgeType, setBridgeType ] = useState<BridgeType>(BridgeType.FLAT_WOOD)
+  const [ cost, setCost ] = useState<BridgeCost | null>(null)
 
   const onSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-  }, [])
+    const cost = calcBridgeCost(bridgeType, length, width)
+    setCost(cost)
+  }, [bridgeType, length, width])
 
   const onReset = useCallback((event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     setLength(0)
     setWidth(1)
     setBridgeType(BridgeType.FLAT_WOOD)
+    setCost(null)
   }, [])
 
   return (
@@ -57,9 +66,7 @@ export default function Bridges() {
               value={bridgeType}
               onChange={(event) => setBridgeType(event.target.value as BridgeType)}>
               {Object.values(BridgeType).map((type) => (
-                <option key={type} value={type}>
-                  {type.slice(0, 1) + type.slice(1).toLowerCase().replace(/_/g, ' ')}
-                </option>
+                <option key={type} value={type}>{formatCode(type)}</option>
               ))}
             </Select>
           </FormControl>
@@ -67,6 +74,24 @@ export default function Bridges() {
         <Button type='submit'>Calculate</Button>
         <Button type='reset' marginLeft={4}>Reset</Button>
       </Box>
+      {cost && (
+        <Card size='sm' marginBottom={8}>
+          <CardHeader>
+            <Heading as='h4' size='sm'>Calculated Cost</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text marginBottom={2}>Total sections: <br/>
+              {cost.sections.map(formatCode).join(', ')}
+            </Text>
+            <Text marginBottom={2}>Total materials:</Text>
+            <SimpleGrid columns={2}>
+              {Array.from(cost.totalMats).map(([code, amount]) => (
+                <Text key={code}>{formatCode(code)}: {amount}</Text>
+              ))}
+            </SimpleGrid>
+          </CardBody>
+        </Card>
+      )}
       <ChakraLink color='teal' as={ReactRouterLink} to='/'>Back to Main Page</ChakraLink>
     </Box>
   )
